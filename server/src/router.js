b727a02bdc6593fcb5b3f9546fc03806a720c8c3
Josh Searle <joshryansearle@gmail.com>
2021-07-07 20:54:46 +1000

    Add routes

--- server/src/router.js
+++ server/src/router.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
+const { authenticateToken } = require('./auth/tokens');
+
 const { createUser, loginUser } = require('./controllers/userControllers');
+const {
+  getChords,
+  addChord,
+  removeChord,
+} = require('./controllers/chordControllers');
 
 router.post('/signup', createUser);
 router.post('/login', loginUser);
+
+router.use(authenticateToken);
+
+router.get('/chords', getChords);
+router.post('/chords', addChord);
+router.delete('/chords', removeChord);
 
 module.exports = router;
