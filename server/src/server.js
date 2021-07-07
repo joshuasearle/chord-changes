ed3615fd69ef2b4394a8b7bb5d8d5fa1d409d4ab
Josh Searle <joshryansearle@gmail.com>
2021-07-07 17:43:43 +1000

    Add router, cors, body parsing, and logging to server

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const router = require('./router');
 
 const port = 5000;
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.use(express.json());
+
+app.use(morgan('tiny'));
+
 app.get('/health', (req, res) => {
   res.status(200).json({ message: 'healthy' });
 });
+
+app.use(router);
 
 const mongoUrl = 'mongodb://localhost:27017/chord-changes';
 
