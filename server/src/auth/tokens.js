7d93267a24b2718b706cfc7937417998df93af32
Josh Searle <joshryansearle@gmail.com>
2021-07-07 17:59:09 +1000

    Add token logic

---
+++ server/src/auth/tokens.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const generateAccessToken = (username) => {
+  return jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '2000h' });
+};
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token === null) return res.status(401).json({ message: 'Invalid token' });
+  jwt.verify(token, process.env.JWT_SECRET, async (err, userToken) => {
+    if (err) return res.status(500).json({ message: 'Server error' });
+    const user = await user.findOne({ username: userToken.username });
+    if (!user) return res.status(401).send('Username does not exist');
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = { generateAccessToken, authenticateToken };
