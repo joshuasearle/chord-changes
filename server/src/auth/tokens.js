3176425afc9297db21fb34beee7f1d3baf5c0b19
Josh Searle <joshryansearle@gmail.com>
2021-07-07 20:53:54 +1000

    Fix typo in tokens

--- server/src/auth/tokens.js
+++ server/src/auth/tokens.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/userModel');
 
 const generateAccessToken = (username) => {
   return jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '2000h' });
@@ -11,8 +11,10 @@ const authenticateToken = (req, res, next) => {
   if (token === null) return res.status(401).json({ message: 'Invalid token' });
   jwt.verify(token, process.env.JWT_SECRET, async (err, userToken) => {
     if (err) return res.status(500).json({ message: 'Server error' });
-    const user = await user.findOne({ username: userToken.username });
-    if (!user) return res.status(401).send('Username does not exist');
+    const user = await User.findOne({ username: userToken.username });
+    if (!user) {
+      return res.status(401).json({ message: 'Username does not exist' });
+    }
     req.user = user;
     next();
   });
